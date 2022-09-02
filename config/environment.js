c77d7ee363bb0a6528c985571ea16503dc175e7f
arthurariza <tucaariza@gmail.com>
2022-09-02 13:37:54 -0300

    fix: disable mirage on dev and prod

--- config/environment.js
+++ config/environment.js
@@ -25,6 +25,9 @@ module.exports = function (environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
+    ENV['ember-cli-mirage'] = {
+      enabled: false,
+    };
   }
 
   if (environment === 'test') {
@@ -41,6 +44,9 @@ module.exports = function (environment) {
 
   if (environment === 'production') {
     // here you can enable a production-specific feature
+    ENV['ember-cli-mirage'] = {
+      enabled: false,
+    };
   }
 
   return ENV;

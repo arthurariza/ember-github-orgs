8bbb2b51d8ed3e59f09a8a467b1a8411659d3f6e
arthurariza <tucaariza@gmail.com>
2022-09-02 16:21:25 -0300

    feat: filter repositories

---
+++ app/components/repositories/filter.js
@@ -0,0 +1,18 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+
+export default class RepositoriesFilterComponent extends Component {
+  @tracked language = '';
+
+  get filtered() {
+    let { repositories } = this.args;
+
+    let regex = new RegExp(this.language.toLowerCase());
+
+    repositories = repositories.filter(({ language }) =>
+      language?.toLowerCase().match(regex)
+    );
+
+    return repositories;
+  }
+}
--- app/routes/organizations/repositories.js
+++ app/routes/organizations/repositories.js
@@ -5,9 +5,7 @@ export default class OrganizationsRepositoriesRoute extends Route {
   async model() {
     let { login } = this.modelFor('organizations');
 
-    const res = await fetch(
-      `https://api.github.com/orgs/${login}/repos?per_page=100`
-    );
+    const res = await fetch(`https://api.github.com/orgs/${login}/repos`);
     const repositories = await res.json();
 
     return repositories;
